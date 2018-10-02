4d50323d1b30bac1411199498d9b180ff19e1f61
niel905g <niel905g@edu.easj.dk>
2018-10-02 04:58:24 +0200

    Clean up check

--- src/niel905g/Main.java
+++ src/niel905g/Main.java
@@ -2,11 +2,11 @@ package niel905g;
 
 public class Main {
 
-        public static void main( String[] argv ) {
+        public static void main( String[] arg ) {
             // Opbygning af objektsystem
             Linie line = new Linie( new Punkt( 3, 0 ), new Punkt( 0, 4 ) );
 
-            // Klient
+            // Client
             System.out.println( line.length() );
         }
     }
